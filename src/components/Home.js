3536c52531b43992dd10010c3f7a4fc9c2d09f15
Stephany <nico96851@gmail.com>
2017-12-03 14:24:56 -0800

    add tab style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+
+const TabPane = Tabs.TabPane;
+
+const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+
     render() {
         return (
-            <div>This is Home!</div>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Tab 1" key="1">
+                    Content of Tab Pane 1
+                </TabPane>
+                <TabPane tab="Tab 2" key="2">
+                    Content of Tab Pane 2
+                </TabPane>
+            </Tabs>
         );
     }
 }
